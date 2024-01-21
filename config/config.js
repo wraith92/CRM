0294579a4a6b36880293f98f169b345f257938b8
unknown <bensalahabdelrrahman1@gmail.com>
2024-01-21 19:40:45 +0100

    commit changement de db

--- config/config.js
+++ config/config.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "",
-  DB: "testdb",
+  HOST: process.env.DB_HOST,
+  USER: process.env.DB_USER,
+  PASSWORD: process.env.DB_PASSWORD,
+  DB: process.env.DB_NAME,
   dialect: "mysql",
   pool: {
     max: 5,
--- index.js
+++ index.js
@@ -124,7 +124,6 @@ const connection = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT
 });
 
 connection.connect(function(err) {
